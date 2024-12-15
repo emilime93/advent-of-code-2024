fd9a17e03c5658dafcedd01f8fb049b1db141c41
Emil Lindholm Brandt <emil93@gmail.com>
2024-12-15 17:53:04 +0100

    Day 7 part 2. Took a while to find the 'mul edge case'

--- cmd/day_07/main.go
+++ cmd/day_07/main.go
@@ -9,8 +9,21 @@ import (
 
 func main() {
 	input := input.ReadFile("cmd/day_07/input.txt")
-	result := SumValidEquationResults(input)
-	fmt.Printf("Sum of valid equations: %d\n", result)
+	result1 := SumValidEquationResults(input)
+	result2 := SumValidEquationResults2(input)
+	fmt.Printf("Sum of valid equations: %d\n", result1)
+	fmt.Printf("Sum of valid equations (part 2): %d\n", result2)
+}
+
+func SumValidEquationResults2(input string) int {
+	equations := inputToEquations(input)
+	sum := 0
+	for _, equation := range equations {
+		if equation.isValidWithConcatenation() {
+			sum += equation.result
+		}
+	}
+	return sum
 }
 
 func SumValidEquationResults(input string) int {
@@ -53,7 +66,37 @@ func recursiveIsValid(goalResult int, operands []int, result int) bool {
 		return goalResult == result
 	}
 	addResult := result + operands[0]
-	mulResult := result * operands[0]
+	// You may not "cancel" the first number by multiplying it with 0...
+	// This is an effect of me starting the reccursion with 0
+	var mulResult int
+	if result == 0 {
+		mulResult = operands[0]
+	} else {
+		mulResult = result * operands[0]
+	}
 	return recursiveIsValid(goalResult, operands[1:], addResult) ||
 		recursiveIsValid(goalResult, operands[1:], mulResult)
+}
+
+func (e *Equation) isValidWithConcatenation() bool {
+	return recursiveIsValidWithConcatenation(e.result, e.operands, 0, 0)
+}
+
+func recursiveIsValidWithConcatenation(goalResult int, operands []int, result int, depth int) bool {
+	if len(operands) == 0 {
+		return goalResult == result
+	}
+	addResult := result + operands[0]
+	// You may not "cancel" the first number by multiplying it with 0...
+	// This is an effect of me starting the reccursion with 0
+	var mulResult int
+	if result == 0 {
+		mulResult = operands[0]
+	} else {
+		mulResult = result * operands[0]
+	}
+	concatResult := util.ToInt(fmt.Sprintf("%d%d", result, operands[0]))
+	return recursiveIsValidWithConcatenation(goalResult, operands[1:], addResult, depth+1) ||
+		recursiveIsValidWithConcatenation(goalResult, operands[1:], mulResult, depth+1) ||
+		recursiveIsValidWithConcatenation(goalResult, operands[1:], concatResult, depth+1)
 }
--- cmd/day_07/main_test.go
+++ cmd/day_07/main_test.go
@@ -20,3 +20,22 @@ func TestSumValidEquationResults(t *testing.T) {
 		t.Errorf("Expected 3749, got %d", result)
 	}
 }
+
+func TestSumValidEquationResultsPart2(t *testing.T) {
+	testInput := `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+3: 1 2 1000
+100: 50 50 2`
+
+	result := SumValidEquationResults2(testInput)
+	if result != 11387 {
+		t.Errorf("Expected 11387, got %d", result)
+	}
+}
